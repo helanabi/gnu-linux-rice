d537146e51a4faaae17cff05b59dc173d9c633d0
Hassan El anabi <abulkeram@proton.me>
2025-10-31 00:38:28 +0000

    Initial commit: my i3 + Neovim rice

---
+++ install.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+CONFIG="$HOME/.config"
+
+mkdir -p "$CONFIG/i3"
+ln -s "$PWD/i3" "$CONFIG/i3/config"
+
+mkdir -p "$CONFIG/i3status"
+ln -s "$PWD/i3status" "$CONFIG/i3status/config"
+
+mkdir -p "$CONFIG/nvim"
+ln -s "$PWD/nvim" "$CONFIG/nvim/init.vim"
+
