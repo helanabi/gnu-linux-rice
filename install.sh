ca18dd248a80391585cbe0914d5e059aac1bae9e
Hassan El anabi <abulkeram@proton.me>
2025-11-05 02:06:17 +0000

    Add xfce4-terminal config

--- install.sh
+++ install.sh
@@ -12,3 +12,7 @@ mkdir -p "$CONFIG/nvim"
 ln -s "$PWD/nvim" "$CONFIG/nvim/init.vim"
 
 ln -s "$PWD/git" "$HOME/.gitconfig"
+
+XFCE_CONF="$CONFIG/xfce4/xfconf/xfce-perchannel-xml/"
+mkdir -p "$XFCE_CONF"
+ln -s "$PWD/xfce4-terminal" "$XFCE_CONF/xfce4-terminal.xml"
