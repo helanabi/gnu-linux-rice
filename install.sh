136735e3d7d60bda65f041d3d7082b2c73817a6e
Hassan El anabi <abulkeram@proton.me>
2025-12-07 23:49:58 +0000

    Make install.sh install packages

--- install.sh
+++ install.sh
@@ -2,6 +2,13 @@
 
 CONFIG="$HOME/.config"
 
+## System-wide config ##
+
+sudo pacman -Syu --needed $(<pkgs)
+sudo cp "$PWD/xorg-keyboard" "/etc/X11/xorg.conf.d/00-keyboard.conf"
+
+## User config ##
+
 mkdir -p "$CONFIG/i3"
 ln -s "$PWD/i3" "$CONFIG/i3/config"
 
@@ -18,7 +25,3 @@ mkdir -p "$XFCE_CONF"
 ln -s "$PWD/xfce4-terminal" "$XFCE_CONF/xfce4-terminal.xml"
 
 ln -s "$PWD/emacs" "$HOME/.emacs"
-
-# System-wide config
-
-sudo cp "$PWD/xorg-keyboard" "/etc/X11/xorg.conf.d/00-keyboard.conf"
